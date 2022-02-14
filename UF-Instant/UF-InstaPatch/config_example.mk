dd5f47c538715830fbb59f705984dd39e09550ab
Röbke Geenen <robke@insaniac.net>
2022-02-15 00:11:01 +0100

    Change default watchdog disabling behavior to not disable the watchdog and added the configuration option to the example config file

--- UF-Instant/UF-InstaPatch/config_example.mk
+++ UF-Instant/UF-InstaPatch/config_example.mk
@@ -1,3 +1,4 @@
-OLT_FW_URL := https://dl.ui.com/firmwares/ufiber/UF-OLT.v4.3.1.3909.210421.0814.tar
-GPON_SN    := AAAAxxxxxxxx
-GPON_MAC   := xx:xx:xx:xx:xx:xx
+OLT_FW_URL       := https://dl.ui.com/firmwares/ufiber/UF-OLT.v4.3.1.3909.210421.0814.tar
+GPON_SN          := AAAAxxxxxxxx
+GPON_MAC         := xx:xx:xx:xx:xx:xx
+DISABLE_WATCHDOG := 0
